f545327f19307adb73ca757798c0c011a52c92e1
Weaver Goldman <we.goldm@gmail.com>
2022-09-03 12:34:07 -0400

    Refactored the moving aim strategy and removed variable distance strategy.

--- src/main/java/frc/robot/strategies/MovingAimStrategy.java
+++ src/main/java/frc/robot/strategies/MovingAimStrategy.java
@@ -6,19 +6,19 @@ import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import frc.robot.Constants.GlobalConstants;
 import frc.robot.Regressions;
 import frc.robot.ShootingSuperstructure;
 import frc.robot.subsystems.ShooterSubsystem;
 import frc.robot.subsystems.SwerveDriveSubsystem;
 import frc.robot.util.InterpolatableDouble;
-import frc.robot.util.ObjectTracker;
 import frc.robot.util.ShooterState;
-
 import java.util.Optional;
 
-public class MovingAimStrategy implements VariableDistanceStrategy {
+public class MovingAimStrategy implements LimelightAimStrategy {
 
     private static double LIMELIGHT_HORIZONTAL_ERROR = 0.1;
 
@@ -26,52 +26,63 @@ public class MovingAimStrategy implements VariableDistanceStrategy {
     private SwerveDriveSubsystem swerveDriveSubsystem;
     private ShooterSubsystem shooterSubsystem;
     private PIDController pidController;
-    private Pose2d currentPosition;
-    private ObjectTracker goalTracker = new ObjectTracker();
+    private Pose2d robotFieldPosition;
     private Rotation2d currentTargetRotation = new Rotation2d();
 
     private InterpolatingMap<InterpolatableDouble> timingMap = Regressions.getShootingTimeMap();
 
     public MovingAimStrategy(ShootingSuperstructure shootingSuperstructure, PIDController pidController) {
         this.shootingSuperstructure = shootingSuperstructure;
+        //PID controller should wrap, if it doesn't things will break
         this.pidController = pidController;
         swerveDriveSubsystem = shootingSuperstructure.getSwerveDriveSubsystem();
     }
 
-    public void updatePositionLimelight() {
-        Optional<Pose2d> limelightPos = shootingSuperstructure.getLimelightPoseEstimate();
-        if (limelightPos.isPresent()) {
-            currentPosition =
-                    goalTracker.calculate(limelightPos.orElseThrow(), swerveDriveSubsystem.getGyroRotation2d());
+    private void updatePositionLimelight() {
+        Optional<Pose2d> robotLimelightRelativePose = shootingSuperstructure.getLimelightPoseEstimate();
+
+        if (robotLimelightRelativePose.isPresent()) {
+            robotFieldPosition = robotLimelightRelativePose.orElseThrow();
         } else {
-            currentPosition = goalTracker.calculate(swerveDriveSubsystem.getVelocity());
+            robotFieldPosition = estimateFutureRobotPosition(0.05);
         }
     }
 
     private double predictShotTime() {
-        Translation2d relativeGoalPosition = GlobalConstants.goalLocation.minus(currentPosition.getTranslation());
+        Translation2d relativeGoalPosition = GlobalConstants.goalLocation.minus(robotFieldPosition.getTranslation());
         double distance = relativeGoalPosition.getNorm();
         double timeForShot = timingMap.get(distance).value;
         // want to add ability to account for robot rotation distance maybe? probably not super neccesary.
         return timeForShot;
     }
 
-    private Pose2d getRelativeFutureGoalPosition(double timeToFuture) {
-        return goalTracker.predict(timeToFuture);
+    private Pose2d estimateRelativeFutureGoalPosition(double timeToFuture) {
+        Pose2d futureRobotPosition = estimateFutureRobotPosition(timeToFuture);
+        return new Pose2d(
+                GlobalConstants.goalLocation.minus(futureRobotPosition.getTranslation()),
+                futureRobotPosition.getRotation().times(-1));
+    }
+
+    private Pose2d estimateFutureRobotPosition(double timeToFuture) {
+        ChassisSpeeds velocityChassisSpeeds = swerveDriveSubsystem.getVelocity();
+        Transform2d velocityTransform2d = new Transform2d(
+                new Translation2d(velocityChassisSpeeds.vxMetersPerSecond, velocityChassisSpeeds.vyMetersPerSecond),
+                new Rotation2d(velocityChassisSpeeds.omegaRadiansPerSecond));
+        return robotFieldPosition.plus(velocityTransform2d.times(timeToFuture));
     }
 
     private Rotation2d getRobotGoalRotation2d() {
         double predictedShotTime = predictShotTime();
-        Pose2d predictedFuturePose2d = getRelativeFutureGoalPosition(predictedShotTime);
+        Pose2d predictedFuturePose2d = estimateRelativeFutureGoalPosition(predictedShotTime);
         double angle = Math.atan2(predictedFuturePose2d.getX(), predictedFuturePose2d.getY());
         return new Rotation2d(angle);
     }
 
     public double calculateRotationalVelocity() {
-        updatePositionLimelight();
         double robotRotation = swerveDriveSubsystem.getGyroRotation2d().getRadians();
         currentTargetRotation = getRobotGoalRotation2d();
-        return pidController.calculate(robotRotation, currentTargetRotation.getRadians());
+        double rotationOutput = pidController.calculate(robotRotation, currentTargetRotation.getRadians());
+        return -1 * rotationOutput * SwerveDriveSubsystem.MAX_ANGULAR_VELOCITY;
     }
 
     public boolean isAimed() {
@@ -82,9 +93,13 @@ public class MovingAimStrategy implements VariableDistanceStrategy {
                 LIMELIGHT_HORIZONTAL_ERROR);
     }
 
-    public ShooterState getShooterState() {
+    public double calculateShotDistance() {
         double predictedShotTime = predictShotTime();
-        Pose2d predictedFuturePose2d = getRelativeFutureGoalPosition(predictedShotTime);
-        return shooterSubsystem.calculateShooterStateForDistance(predictedFuturePose2d.getTranslation().getNorm());
+        Pose2d predictedFutureGoalPose2d = estimateRelativeFutureGoalPosition(predictedShotTime);
+        return predictedFutureGoalPose2d.getTranslation().getNorm();
+    }
+
+    public void update() {
+        updatePositionLimelight();
     }
 }
--- src/main/java/frc/robot/strategies/VariableDistanceStrategy.java
+++
@@ -1,7 +0,0 @@
-package frc.robot.strategies;
-
-import frc.robot.util.ShooterState;
-
-public interface VariableDistanceStrategy extends LimelightAimStrategy {
-    public ShooterState getShooterState();
-}
