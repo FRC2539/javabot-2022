b5c8023d6b49afc1c6e220123480d958042504f9
Ben <bbistline02@gmail.com>
2022-09-07 20:35:43 -0400

    Cleaned up MovingAimStrategy

--- src/main/java/frc/robot/strategies/MovingAimStrategy.java
+++ src/main/java/frc/robot/strategies/MovingAimStrategy.java
@@ -14,16 +14,16 @@ import frc.robot.Regressions;
 import frc.robot.ShootingSuperstructure;
 import frc.robot.subsystems.SwerveDriveSubsystem;
 import frc.robot.util.InterpolatableDouble;
-import java.util.Optional;
 
-public class MovingAimStrategy implements LimelightAimStrategy {
+public class MovingAimStrategy {
+    // for using, call update before using other functions
 
     private static double LIMELIGHT_HORIZONTAL_ERROR = 0.1;
 
     private ShootingSuperstructure shootingSuperstructure;
     private SwerveDriveSubsystem swerveDriveSubsystem;
     private PIDController pidController;
-    private Pose2d robotFieldPosition;
+    private Translation2d relativeVirtualGoalPosition;
     private Rotation2d currentTargetRotation = new Rotation2d();
 
     private InterpolatingMap<InterpolatableDouble> timingMap = Regressions.getShootingTimeMap();
@@ -35,32 +35,12 @@ public class MovingAimStrategy implements LimelightAimStrategy {
         swerveDriveSubsystem = shootingSuperstructure.getSwerveDriveSubsystem();
     }
 
-    private void updatePositionLimelight() {
-        Optional<Pose2d> robotLimelightRelativePose = shootingSuperstructure.getLimelightPoseEstimate();
-
-        if (robotLimelightRelativePose.isPresent()) {
-            robotFieldPosition = robotLimelightRelativePose.orElseThrow();
-        } else {
-            robotFieldPosition = estimateFutureRobotPosition(0.05);
-        }
+    private Translation2d estimateRelativeFutureGoalTranslation(double timeToFuture, Pose2d currentRobotPose) {
+        Pose2d futureRobotPosition = estimateFutureRobotPosition(timeToFuture, currentRobotPose);
+        return GlobalConstants.goalLocation.minus(futureRobotPosition.getTranslation());
     }
 
-    private double predictShotTime() {
-        Translation2d relativeGoalPosition = GlobalConstants.goalLocation.minus(robotFieldPosition.getTranslation());
-        double distance = relativeGoalPosition.getNorm();
-        double timeForShot = timingMap.get(distance).value;
-        // want to add ability to account for robot rotation distance maybe? probably not super neccesary.
-        return timeForShot;
-    }
-
-    private Pose2d estimateRelativeFutureGoalPosition(double timeToFuture) {
-        Pose2d futureRobotPosition = estimateFutureRobotPosition(timeToFuture);
-        return new Pose2d(
-                GlobalConstants.goalLocation.minus(futureRobotPosition.getTranslation()),
-                futureRobotPosition.getRotation().times(-1));
-    }
-
-    private Pose2d estimateFutureRobotPosition(double timeToFuture) {
+    private Pose2d estimateFutureRobotPosition(double timeToFuture, Pose2d robotFieldPosition) {
         ChassisSpeeds velocityChassisSpeeds = swerveDriveSubsystem.getVelocity();
         Transform2d velocityTransform2d = new Transform2d(
                 new Translation2d(velocityChassisSpeeds.vxMetersPerSecond, velocityChassisSpeeds.vyMetersPerSecond),
@@ -68,18 +48,10 @@ public class MovingAimStrategy implements LimelightAimStrategy {
         return robotFieldPosition.plus(velocityTransform2d.times(timeToFuture));
     }
 
-    private Rotation2d getRobotGoalRotation2d() {
-        double predictedShotTime = predictShotTime();
-        Pose2d predictedFuturePose2d = estimateRelativeFutureGoalPosition(predictedShotTime);
-        double angle = Math.atan2(predictedFuturePose2d.getX(), predictedFuturePose2d.getY());
-        return new Rotation2d(angle);
-    }
-
     public double calculateRotationalVelocity() {
         double robotRotation = swerveDriveSubsystem.getGyroRotation2d().getRadians();
-        currentTargetRotation = getRobotGoalRotation2d();
         double rotationOutput = pidController.calculate(robotRotation, currentTargetRotation.getRadians());
-        return -1 * rotationOutput * SwerveDriveSubsystem.MAX_ANGULAR_VELOCITY;
+        return rotationOutput * SwerveDriveSubsystem.MAX_ANGULAR_VELOCITY;
     }
 
     public boolean isAimed() {
@@ -91,12 +63,22 @@ public class MovingAimStrategy implements LimelightAimStrategy {
     }
 
     public double calculateShotDistance() {
-        double predictedShotTime = predictShotTime();
-        Pose2d predictedFutureGoalPose2d = estimateRelativeFutureGoalPosition(predictedShotTime);
-        return predictedFutureGoalPose2d.getTranslation().getNorm();
+        return relativeVirtualGoalPosition.getNorm();
     }
 
     public void update() {
-        updatePositionLimelight();
+        // updates the field position
+        Pose2d robotFieldPosition =
+                shootingSuperstructure.getLimelightPoseEstimate().orElse(swerveDriveSubsystem.getPose());
+
+        // claculates the relative virtual goal position
+        Translation2d relativeGoalPosition = GlobalConstants.goalLocation.minus(robotFieldPosition.getTranslation());
+        double distance = relativeGoalPosition.getNorm();
+        double predictedShotTime = timingMap.get(distance).value;
+        relativeVirtualGoalPosition = estimateRelativeFutureGoalTranslation(predictedShotTime, robotFieldPosition);
+
+        // this has - to accound for the fact that 180 is shooter
+        double robotGoalAngle = Math.atan2(-relativeVirtualGoalPosition.getX(), -relativeVirtualGoalPosition.getY());
+        currentTargetRotation = new Rotation2d(robotGoalAngle);
     }
 }
